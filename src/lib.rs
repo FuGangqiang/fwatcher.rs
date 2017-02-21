6fcb72c7bd6ec6b6bc89fbd10c4e70b0fd2f6fe6
FuGangqiang <fu_gangqiang@qq.com>
2017-02-21 09:31:20 +0800

    fix doc link

--- src/lib.rs
+++ src/lib.rs
@@ -36,7 +36,7 @@
 
 #![doc(html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "https://www.rust-lang.org/favicon.ico",
-       html_root_url = "https://fugangqiang.github.io/doc/fwatcher")]
+       html_root_url = "https://fugangqiang.github.io/docs/fwatcher.rs/fwatcher/")]
 
 extern crate getopts;
 extern crate glob;
